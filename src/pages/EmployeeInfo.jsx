acf96808d16f99457434e655cebe7940b116ce90
Mohammad <mmh.ksx.2002@gmail.com>
2023-09-20 11:53:26 +0330

    Fix Bugs

--- src/pages/EmployeeInfo.jsx
+++ src/pages/EmployeeInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Header from '../components/EmployeeInfo/Header';
 import About from '../components/EmployeeInfo/About';
 import Skills from '../components/EmployeeInfo/Skills';
@@ -7,43 +7,54 @@ import ContactDeveloper from '../components/EmployeeInfo/ContactDeveloper';
 import Footer from '../components/EmployeeInfo/Footer';
 
 function EmployeeInfo() {
-    const [headerShow, setHeaderShow] = useState('sticky');
-    const [scrollToTop, setScrollToTop] = useState(false);
-    
+    const headerElementRef = useRef();
+    const scrollToTopRef = useRef();
+
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' })
     }, [])
 
     useEffect(() => {
-        window.addEventListener('scroll', () => {
+        const scrollToTopFunc = () => {
             if (window.scrollY > 500) {
-                setScrollToTop(true)
+                scrollToTopRef.current.classList.add("translate-y-0")
+                scrollToTopRef.current.classList.remove("translate-y-[120px]")
             } else {
-                setScrollToTop(false)
+                scrollToTopRef.current.classList.add("translate-y-[120px]")
+                scrollToTopRef.current.classList.remove("translate-y-0")
             }
-        })
+        }
+        window.addEventListener('scroll', scrollToTopFunc)
+
+        return () => window.removeEventListener('scroll', scrollToTopFunc)
     }, []);
 
     useEffect(() => {
-        window.onscroll = function (e) {
-            if (this.oldScroll > this.scrollY || this.oldScroll < 100) {
-                setHeaderShow('header-animation-show')
-            } else if (this.oldScroll < this.scrollY) {
-                setHeaderShow('header-animation-hide');
+        var prevScrollPos = window.scrollY;
+        const headerFunc = () => {
+            var currentScrollPos = window.scrollY;
+            if (prevScrollPos > currentScrollPos) {
+                headerElementRef.current.classList.add("header-animation-show")
+                headerElementRef.current.classList.remove("header-animation-hide")
+            } else {
+                headerElementRef.current.classList.add("header-animation-hide")
+                headerElementRef.current.classList.remove("header-animation-show")
             }
-            this.oldScroll = this.scrollY;
+            prevScrollPos = currentScrollPos;
         }
-    }, []);
+        window.addEventListener('scroll', headerFunc)
 
+        return () => window.removeEventListener('scroll', headerFunc)
+    }, []);
     return (
         <>
-            <button className={`fixed bottom-10 left-10 z-50 bg-blue-700 p-2 text-white cursor-pointer transition-all duration-500 rounded-full ${scrollToTop ? "translate-y-0" : "translate-y-[120px]"}`} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+            <button ref={scrollToTopRef} className="fixed bottom-10 left-10 z-50 bg-blue-700 p-2 text-white cursor-pointer transition-all duration-500 rounded-full" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
                 </svg>
             </button>
 
-            <header className={`sticky top-0 z-50 transition-all duration-500 ${headerShow}`}>
+            <header ref={headerElementRef} className="sticky top-0 z-50 transition-all duration-500">
                 <Header />
             </header>
             <main>
--- src/pages/Organization.jsx
+++ src/pages/Organization.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import Header from '../components/Organization/Header'
 import About from '../components/Organization/About'
 import Employee from '../components/Organization/Employee'
@@ -7,43 +7,55 @@ import ContactUs from '../components/Organization/ContactUs'
 import Footer from '../components/Organization/Footer'
 
 function Organization() {
-  const [headerShow, setHeaderShow] = useState('sticky');
-  const [scrollToTop, setScrollToTop] = useState(false);
+  const headerElementRef = useRef();
+  const scrollToTopRef = useRef();
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }, [])
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
+    const scrollToTopFunc = () => {
       if (window.scrollY > 500) {
-        setScrollToTop(true)
+        scrollToTopRef.current.classList.add("translate-y-0")
+        scrollToTopRef.current.classList.remove("translate-y-[120px]")
       } else {
-        setScrollToTop(false)
+        scrollToTopRef.current.classList.add("translate-y-[120px]")
+        scrollToTopRef.current.classList.remove("translate-y-0")
       }
-    })
+    }
+    window.addEventListener('scroll', scrollToTopFunc)
+
+    return () => window.removeEventListener('scroll', scrollToTopFunc)
   }, []);
 
   useEffect(() => {
-    window.onscroll = function (e) {
-      if (this.oldScroll > this.scrollY || this.oldScroll < 100) {
-        setHeaderShow('header-animation-show')
-      } else if (this.oldScroll < this.scrollY) {
-        setHeaderShow('header-animation-hide');
+    var prevScrollPos = window.scrollY;
+    const headerFunc = () => {
+      var currentScrollPos = window.scrollY;
+      if (prevScrollPos > currentScrollPos) {
+        headerElementRef.current.classList.add("header-animation-show")
+        headerElementRef.current.classList.remove("header-animation-hide")
+      } else {
+        headerElementRef.current.classList.add("header-animation-hide")
+        headerElementRef.current.classList.remove("header-animation-show")
       }
-      this.oldScroll = this.scrollY;
+      prevScrollPos = currentScrollPos;
     }
+    window.addEventListener('scroll', headerFunc)
+
+    return () => window.removeEventListener('scroll', headerFunc)
   }, []);
 
   return (
     <>
-      <button className={`fixed bottom-10 left-10 z-50 bg-blue-700 p-2 text-white cursor-pointer transition-all duration-500 rounded-full ${scrollToTop ? "translate-y-0" : "translate-y-[120px]"}`} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+      <button ref={scrollToTopRef} className="fixed bottom-10 left-10 z-50 bg-blue-700 p-2 text-white cursor-pointer transition-all duration-500 rounded-full" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-8 h-8">
           <path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
         </svg>
       </button>
 
-      <header className={`sticky top-0 z-50 transition-all duration-500 ${headerShow}`}>
+      <header ref={headerElementRef} className="sticky top-0 z-50 transition-all duration-500">
         <Header />
       </header>
       <main>
